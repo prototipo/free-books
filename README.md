e75a2879098597c8b0767ed9bb297f2ed0b7cb6c
David <david@pque>
2020-04-30 16:28:07 +0200

    Added README.md

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # free-books
 Elsevier and Springer have made available free books because of the COVID-19 pandemia. This script makes easy to search for books and to download them
+
+Python 3 is needed. Installation is as easy as downloading the script and having installed the following modules:
+
+* pandas
+* urllib3
+* beautifulsoup4
+
+The script has fundamentally two uses:
+
+## Searching
+
+In order to search a book, the instruction is:
+
+```shell
+python free-books.py -s '.*Artificial.*'
+```
+
+Then, the script return the following lines:
+
+```shell
+Searching for books that match the regular expression...
+
+* Elsevier matches:
+
+                                  Book Title  Year          Imprint
+11  Artificial Intelligence: A New Synthesis  1998  Morgan Kaufmann
+
+* Springer matches:
+
+                                  Book Title  Copyright Year          Author
+301  Introduction to Artificial Intelligence            2017  Wolfgang Ertel
+```
+
+## Downloading
+
+Once we know the index of the book we want to download, we only have to use the instruction as follows:
+
+```shell
+python free-books.py -d s301
+```
+
+If the index starts with an *s*, then the books comes from the Springer library. Elsevier downloads still not implemented *Coming soon*.
--- free-books.py
+++ free-books.py
@@ -1,4 +1,89 @@
 #!/usr/bin/python
 # -*- encoding: utf-8 -*-
 
+import sys
+import re
+import argparse
+import pandas as pd
+import urllib3 as urllib
+from bs4 import BeautifulSoup
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='\
+This program makes easier to search and download\
+the books that both Elsevier and Springer have made\
+available for free because of the COVID-19 pandemia.\
+')
+    parser.add_argument('-s', '--search',\
+                        help='Search a book with regular expressions', \
+                        action='store')
+    parser.add_argument('-d', '--download',\
+                        help='Download books passing the with regular expressions', \
+                        action='store', nargs = max(1,(len(sys.argv) - 2)))
+    args = parser.parse_args()
+    return args
+
+def search(args):
+    print('Searching for books that match the regular expression...')
+    data_elsevier = pd.read_csv('elsevier-books.csv', sep=';')
+    df_elsevier = data_elsevier[data_elsevier['Book Title'].str.match(r'{}'.format(args))]
+    if len(df_elsevier):
+        print('\n* Elsevier matches:\n')
+        print(df_elsevier[['Book Title', 'Year', 'Imprint']])
+    data_springer = pd.read_csv('springer-books.csv', sep=';')
+    df_springer = data_springer[data_springer['Book Title'].str.match(r'{}'.format(args))]
+    if len(df_springer):
+        print('\n* Springer matches:\n')
+        print(df_springer[['Book Title', 'Copyright Year', 'Author']])
+
+def download(args):
+    for index in args:
+        if index.startswith('s'):
+            print('Springer book')
+            index = int(index[1:])
+            download_springer(index)
+        elif index.startswith('e'):
+            print('Elsevier book')
+            index = int(index[1:])
+            download_elsevier(index)
+
+def download_springer(index):
+    data_springer = pd.read_csv('springer-books.csv', sep=';')
+    if index < len(data_springer):
+        book = data_springer.loc[index]
+        print('Downloading {}...'.format(book['Book Title']))
+        url = book['OpenURL']
+        http = urllib.PoolManager()
+        response = http.request('GET', url)
+        html = BeautifulSoup(response.data, features='html.parser')
+        div_book = html.findAll('a', {'class' : 'test-bookpdf-link'})[0]
+        url_book = div_book['href']
+        url_book = 'https://link.springer.com' + url_book
+        filename = '{}.pdf'.format(book['Book Title'])
+        response.release_conn()
+        r = http.request('GET', url_book, preload_content=False)
+        with open(filename, 'wb') as out:
+            while True:
+                data = r.read()
+                if not data:
+                    break
+                out.write(data)
+        r.release_conn()
+    else:
+        print('Not found')
+    pass
+
+def download_elsevier(index):
+    data_elsevier = pd.read_csv('elsevier-books.csv', sep=';')
+    pass
+
+if __name__ == '__main__':
+    args = parse_arguments()
+    if args.search:
+        search(sys.argv[2])
+    elif args.download:
+        download(sys.argv[2:])
+    else:
+        print('Please, use free-books.py -h for more help.')
+
 
